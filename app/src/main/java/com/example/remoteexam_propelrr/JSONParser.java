75c15c3f28e83ff031d95cf3aecc4621ee8c0154
Rennan Amoguis <amoguis.rennan@gmail.com>
2023-01-05 01:50:11 +0800

    first commit

---
+++ app/src/main/java/com/example/remoteexam_propelrr/JSONParser.java
@@ -0,0 +1,111 @@
+package com.example.remoteexam_propelrr;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+    int check=0;
+    // constructor
+    public JSONParser() {
+
+    }
+
+    // function get json from url
+    // by making HTTP POST or GET method
+    public JSONObject makeHttpRequest(String url, String method,
+                                      List<NameValuePair> params) {
+
+        // Making HTTP request
+        try {
+
+            // check for request method
+            if(method.equals("POST")){
+                // request method is POST
+                // defaultHttpClient
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(url);
+                httpPost.setEntity(new UrlEncodedFormEntity(params));
+
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+
+            }else if(method.equals("GET")){
+                // request method is GET
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                String paramString = URLEncodedUtils.format(params, "utf-8");
+                url += "?" + paramString;
+                HttpGet httpGet = new HttpGet(url);
+
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            Log.e("Buffer Result 1: ",sb.toString());
+            Log.e("Buffer Result 2: ",reader.toString());
+            Log.e("Params: ", params.toString());
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            check=1;
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            if(check == 1) {
+                jObj = null;
+                check=0;
+            }else{
+                jObj = new JSONObject(json);
+            }
+        } catch (JSONException e) {
+            jObj=null;
+            Log.e("JSON Parser", "Error parsing data [" + e.toString()+"]"+json);
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+
+}
--- app/src/main/java/com/example/remoteexam_propelrr/MainActivity.java
+++ app/src/main/java/com/example/remoteexam_propelrr/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.remoteexam_propelrr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,12 +16,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String URL_FOR_REGISTRATION = "https://run.mocky.io/v3/a6b6c99a-c4d6-4514-ba9c-90e4957adc76";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_FNAME = "fullname";
 
     com.google.android.material.textfield.TextInputEditText getFullname, getEmail, getPhone;
     com.google.android.material.button.MaterialButton btnSubmit;
@@ -29,66 +42,81 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     String[] gender = {"Male", "Female"};
 
+    JSONParser jsonParser = new JSONParser();
+
+    StringBuffer buffer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getFullname = (com.google.android.material.textfield.TextInputEditText)findViewById(R.id.et_fullname);
-        getEmail = (com.google.android.material.textfield.TextInputEditText)findViewById(R.id.et_email_address);
-        getPhone = (com.google.android.material.textfield.TextInputEditText)findViewById(R.id.et_mobile_no);
-        getDOB = (TextView) findViewById(R.id.tv_dob);
-        getAge = (TextView) findViewById(R.id.tv_age);
-        imgGetDate = (ImageView) findViewById(R.id.img_date_picker);
-        setGender = (Spinner) findViewById(R.id.spinner_gender);
-        btnSubmit = (com.google.android.material.button.MaterialButton)findViewById(R.id.btn_submit);
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
 
-        ArrayAdapter<String> genderAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, gender);
-        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        setGender.setAdapter(genderAdapter);
-        setGender.setOnItemSelectedListener(this);
+            getFullname = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.et_fullname);
+            getEmail = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.et_email_address);
+            getPhone = (com.google.android.material.textfield.TextInputEditText) findViewById(R.id.et_mobile_no);
+            getDOB = (TextView) findViewById(R.id.tv_dob);
+            getAge = (TextView) findViewById(R.id.tv_age);
+            imgGetDate = (ImageView) findViewById(R.id.img_date_picker);
+            setGender = (Spinner) findViewById(R.id.spinner_gender);
+            btnSubmit = (com.google.android.material.button.MaterialButton) findViewById(R.id.btn_submit);
 
-        imgGetDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Calendar calendar = Calendar.getInstance();
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-                int month = calendar.get(Calendar.MONTH);
-                int year = calendar.get(Calendar.YEAR);
-                datePicker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int myYear, int myMonth, int myDay) {
-                        getDOB.setText((myMonth + 1) + "/" + myDay + "/" + myYear);
-                        if(myMonth<month) {
-                            getAge.setText(String.valueOf(year - myYear));
-                        }else{
-                            getAge.setText(String.valueOf((year-1) - myYear));
+            ArrayAdapter<String> genderAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, gender);
+            genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            setGender.setAdapter(genderAdapter);
+            setGender.setOnItemSelectedListener(this);
+
+            imgGetDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Calendar calendar = Calendar.getInstance();
+                    int day = calendar.get(Calendar.DAY_OF_MONTH);
+                    int month = calendar.get(Calendar.MONTH);
+                    int year = calendar.get(Calendar.YEAR);
+                    datePicker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int myYear, int myMonth, int myDay) {
+                            getDOB.setText((myMonth + 1) + "/" + myDay + "/" + myYear);
+                            if (myMonth < month) {
+                                getAge.setText(String.valueOf(year - myYear));
+                            } else {
+                                getAge.setText(String.valueOf((year - 1) - myYear));
+                            }
                         }
-                    }
-                }, year, month, day);
-                datePicker.show();
-            }
-        });
+                    }, year, month, day);
+                    datePicker.show();
+                }
+            });
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(getFullname.getText().toString().equals("") || getEmail.getText().toString().equals("") ||
-                    getPhone.getText().toString().equals("") || getDOB.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(),"All fields are required.",Toast.LENGTH_SHORT).show();
-                }else{
-                    if (!ValidateEmail()) {
-                        getEmail.setError("Invalid Email Format");
-                    }
-                    if (!ValidateNumber() || getPhone.length()<11) {
-                        getPhone.setError("Invalid Phone Number Format");
-                    }
-                    if (!ValidateAge()) {
-                        Toast.makeText(getApplicationContext(), "Age must be 18 or above.", Toast.LENGTH_SHORT).show();
+            btnSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (getFullname.getText().toString().equals("") || getEmail.getText().toString().equals("") ||
+                            getPhone.getText().toString().equals("") || getDOB.getText().toString().equals("")) {
+                        Toast.makeText(getApplicationContext(), "All fields are required.", Toast.LENGTH_SHORT).show();
+                    } else if(!ValidateEmail() || !ValidateNumber() || getPhone.length() < 11 || !ValidateAge()) {
+
+                        if (!ValidateEmail()) {
+                            getEmail.setError("Invalid Email Format");
+                        }
+                        if (!ValidateNumber() || getPhone.length() < 11) {
+                            getPhone.setError("Invalid Phone Number Format");
+                        }
+                        if (!ValidateAge()) {
+                            Toast.makeText(getApplicationContext(), "Age must be 18 or above.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }else{
+                        new RegisterUser().execute();
                     }
                 }
-            }
-        });
+            });
+        }
 
     }
 
@@ -118,6 +146,63 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     public boolean ValidateAge(){
         return Integer.parseInt(getAge.getText().toString()) >= 18;
+    }
+
+    class RegisterUser extends AsyncTask<String, String, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            if(data != null){
+                try{
+                    JSONObject jsonObject = new JSONObject(data);
+                    int success = jsonObject.getInt(TAG_SUCCESS);
+                    if(success==1){
+
+                        buffer = new StringBuffer();
+                        buffer.append("Account Name: " + jsonObject.getString(TAG_FNAME) + "\n");
+                        buffer.append("has been successfully registered.");
+
+                        DialogBox();
+
+                    }else{
+                        buffer = new StringBuffer();
+                        buffer.append("Registration Failed");
+                        DialogBox();
+                    }
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }else{
+                buffer = new StringBuffer();
+                buffer.append("Server no response.");
+                DialogBox();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try{
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                return jsonParser.makeHttpRequest(URL_FOR_REGISTRATION,"POST", params).toString();
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    private void DialogBox(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle("System Message");
+        builder.setMessage(buffer.toString());
+        builder.show();
     }
 
 }
